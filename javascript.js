504433eabb86d933a3afedbc3abf1b56520f93fe
Bogdan Lykhosherstov <bogdan@exdee.ai>
2019-01-23 16:35:24 -0700

    Finished version with JS and JQuery

---
+++ javascript.js
@@ -0,0 +1,37 @@
+var CalgaryLocation = {
+  Name:"Calgary",
+  Latitude: "51.0486",
+  Longitude: "- 114.0708",
+  Population: 1096833,
+  Area: 825.29,
+  Density: "Population/Area"
+};
+var EdmontonLocation = {
+  Name:"Edmonton",
+  Latitude: "53.5444",
+  Longitude: "- 113.4909",
+  Population: 960015,
+  Area: 684.37,
+  Density: "Population/Area"
+};
+
+var circleCalgary = document.getElementById('hoverCircle');
+var circleEdmonton = document.getElementById('hoverCircle2');
+
+var mapBox = document.getElementById('mapBox');
+
+circleCalgary.onmouseover = function() {
+  mapBox.style.color = "#444444";
+  mapBox.innerHTML = `<div class='innerBoxInfo'>Name: Calgary,<br><br>Latitude: 51.0486,<br><br>Population: 1,096,833,<br><br>Longitude: - 114.0708,<br><br>Area: 825.29 SqKM,<br><br>Density: ${CalgaryLocation.Population / CalgaryLocation.Area}</div>`
+}
+circleCalgary.onmouseout = function() {
+  mapBox.innerHTML = '';
+}
+
+circleEdmonton.onmouseover = function() {
+  mapBox.style.color = "#444444";
+  mapBox.innerHTML = `<div class='innerBoxInfo'>Name: Edmonton,<br><br>Latitude: 53.5444,<br><br>Population: 960,015,<br><br>Longitude: - 113.4909,<br><br>Area: 684.37 SqKM,<br><br>Density: ${EdmontonLocation.Population / EdmontonLocation.Area}</div>`
+}
+circleEdmonton.onmouseout = function() {
+  mapBox.innerHTML = '';
+}
